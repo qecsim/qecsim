9124ebf36388084fdeabad3297659b6ca7c74dbf
David Tuckett <dtuc0357@uni.sydney.edu.au>
2020-06-10 14:19:55 +1000

    TODOs

--- qecsim/model.py
+++ qecsim/model.py
@@ -10,7 +10,7 @@ import numpy as np
 from qecsim import paulitools as pt
 from qecsim.error import QecsimException
 
-ATTR_CLI_DESCRIPTION = '__qecsim_cli_desc'
+ATTR_CLI_DESCRIPTION = '__qecsim_cli_desc'  # TODO: consider removing this
 
 
 def cli_description(description):
