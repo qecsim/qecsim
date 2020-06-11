9ad513077e4c014f9efaf30519edcd8174db89bc
David Tuckett <dtuc0357@uni.sydney.edu.au>
2020-06-11 14:12:09 +1000

    docs: configure tox for sphinx

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+import qecsim
+
+# -- Project information --------------------------------------------------
+project = 'qecsim'
+copyright = '2016, David Tuckett'
+author = 'David Tuckett'
+
+version = qecsim.__version__
+release = qecsim.__version__
+
+# -- General configuration ------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    # 'sphinx.ext.viewcode', # adds links to Python source code
+    'sphinx.ext.intersphinx',
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+qecsim - Quantum Error Correction Simulator
+===========================================
+
+qecsim is a Python package for simulating quantum error correction using stabilizer codes.
+
+The Overview section describes how qecsim works, the Usage section explains how to use qecsim, and the Demos
+section shows qecsim in action.
+
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+
+   overview
+
+Indices
+=======
+
+-  :ref:`genindex`
+-  :ref:`modindex`
