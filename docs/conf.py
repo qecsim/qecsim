9965411b0fb7aa7a5aa346273c129e6d0bb8ceb9
David Tuckett <dtuc0357@uni.sydney.edu.au>
2020-06-11 14:19:44 +1000

    docs: configure tox for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,11 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     # 'sphinx.ext.viewcode', # adds links to Python source code
-    'sphinx.ext.intersphinx',
+    # 'sphinx.ext.intersphinx', # adds links to other sphinx docs
 ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+# intersphinx_mapping = {'python': ('https://docs.python.org/3', None)} # adds links to Python docs
 
 # -- Options for HTML output ----------------------------------------------
 
